6fa97026ff191b18da1a1d6a58d8db62241569c3
Rohit <rohit@gmail.com>
2026-08-02 19:26:15 +0530

    fix: resolve CORS issue and remove exposed secrets

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -2,9 +2,11 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
-    withCredentials: true
-})
+  // This will use your Vercel variable in production, or localhost in development
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000", 
+  withCredentials: true
+} )
+
 
 export async function register({ username, email, password }) {
 
